c9e169c1e70592442164c8beb90ceb5b8b10d927
Maria Victoria <marivibilbaog@gmail.com>
2021-07-31 14:10:57 +0200

    Agrego segunda parte del ejercicio

--- week-1/Homework/dom-manipulation-2/js/main.js
+++ week-1/Homework/dom-manipulation-2/js/main.js
@@ -52,3 +52,62 @@ buttonGreen.addEventListener("click", (event) => { //Agrego función del evento 
     buttonDonate.style.backgroundColor = "black";
     buttonVolunteer.style.backgroundColor = "#8c9c08";
 });
+
+/*
+Part 2
+Just below the buttons, there's a form called Register with us.
+Continue working in ./js/main.js to add the following functionality:
+When the submit button is pressed, it should check that all the form fields are valid:
+The Your name, Email and Describe yourself fields need to be non-empty (Hint: their value length has to be greater than zero)
+For the Email field also check if it contains the @ character
+For all the fields that invalid, it should make their background color red. IF all the fields are valid, when you click Submit it should:
+Display an alert to thank you for filling out the form
+Blank out (make empty) all the text fields
+Important hint: In your function that handles clicks on the Submit button you will need to call event.preventDefault() to stop the browser from refreshing the page. To read more on how to do this: https://developer.mozilla.org/en/docs/Web/API/Event/preventDefault
+*/
+
+const mail = document.querySelector("#exampleInputEmail1");
+const name = document.querySelector("#example-text-input");
+const area = document.querySelector("#exampleTextarea");
+const submit = document.querySelector("form button");
+
+function email() {
+    if (mail.value.length > 0 && mail.value.includes("@")){
+        mail.style.border = "";
+    } else {
+        mail.style.border = "2px solid red";
+    }
+}
+
+function ename() {
+    if (name.value.length > 0 ){
+        name.style.border = "";
+    } else {
+        name.style.border = "2px solid red";
+    }
+}
+
+function earea() {
+    if (area.value.length > 0 ){
+        area.style.border = "";
+    } else {
+        area.style.border = "2px solid red";
+    }
+}
+
+submit.addEventListener("click", (event)=>{
+    event.preventDefault();
+    if(mail.value.length > 0 && name.value.length > 0 && area.value.length > 0 && mail.value.includes("@")){
+        mail.value = "";
+        name.value = "";
+        area.value = "";
+        mail.style.border = "1px solid rgba(0,0,0,.15)";
+        name.style.border = "1px solid rgba(0,0,0,.15)";
+        area.style.border = "1px solid rgba(0,0,0,.15)";
+    }else{
+        mail.addEventListener("focusout", email());
+        name.addEventListener("focusout", ename());
+        area.addEventListener("focusout", earea());
+        alert("Debes rellenar todo el formulario");
+    }
+})

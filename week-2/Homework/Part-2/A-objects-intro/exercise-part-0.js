bd39faee91feb17049a179afc3ea10a14fe12ba1
Maria Victoria <marivibilbaog@gmail.com>
2021-08-04 22:19:19 +0200

    Agrego exercise-part-0

--- week-2/Homework/Part-2/A-objects-intro/exercise-part-0.js
+++ week-2/Homework/Part-2/A-objects-intro/exercise-part-0.js
@@ -6,8 +6,25 @@ Try to think of as many properties as you can!
 
 */
 
+let myLaptop = {
+  brand: "Lenovo",
+  screenSize: 14,
+  isTouchScreen: true,
+  sistemaOperativo: "Windows 10",
+  colorOfLaptop: "Gray",
+  hasWebcam: true,
+  modelo: "Ideapad 5",
+  memoriaRam: "8 GB",
+  bitsSistemaOperativo: 64,
+  bluetooth: true,
+  discoDuro: 520,
+}
+
+
+/* Información que ya se encontraba en el ejercicio
 let laptop = {
     brand: "Lenovo",
     screenSize: 13,
     isTouchscreen: true,
   };
+*/

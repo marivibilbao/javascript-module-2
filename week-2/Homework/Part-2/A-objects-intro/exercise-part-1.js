e2f76e54d54735d9a255f02d2c88aedfeec15c44
Maria Victoria <marivibilbaog@gmail.com>
2021-08-04 22:56:47 +0200

    Agrego exercise-part-1

--- week-2/Homework/Part-2/A-objects-intro/exercise-part-1.js
+++ week-2/Homework/Part-2/A-objects-intro/exercise-part-1.js
@@ -17,3 +17,50 @@ var car = {
 
 */
 
+var mobilePhone = {
+    brand: "Xiaomi",
+    model: "Redmi Note 8 Pro",
+    color: "Negro",
+    memoriaRam: "6GB",
+    hasAndroid: true,
+    color: "Negro",
+};
+
+var tv = {
+    brand: "Samsung",
+    screenSize: 32,
+    color: "Negro",
+    resolucionHd: true,
+    smartTv: true,
+    peso: "4.1Kg",
+};
+
+var robotAspirador = {
+    brand: "Lefant",
+    color: "Gris",
+    capacidadLitros: 0.6,
+    nivelRuidoDb: 50, 
+    horasAutonomia: 3,
+    fregasuelos: true,
+    aspira: true,
+};
+
+var aireAcondicionado = {
+    brand: "Hisense",
+    model: "APC12",
+    color: "Blanco",
+    airePortátil: true,
+    deshumidificador: true,
+    numeroVelocidaddes: 3,
+    aireFijoPared: false,
+};
+
+var impresora = {
+    brand: "EPSON",
+    model: "ECOTANK ET-2714",
+    color: "Negro",
+    multifuncion: true,
+    tintaInyeccion: true,
+    capacidadHojas: 100,
+    conexionWifi: true,
+};

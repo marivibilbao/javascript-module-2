c4c148a5e672e071229b16df25262caf8486452b
Maria Victoria <marivibilbaog@gmail.com>
2021-08-04 22:28:44 +0200

    A

--- week-2/Homework/Part-2/A-objects-intro/exercise-part-2.js
+++ week-2/Homework/Part-2/A-objects-intro/exercise-part-2.js
@@ -5,17 +5,18 @@ The objects below have some syntax issues - try and fix them all!
 */
 
 var kitten = {
-    fur colour: "orange",
-    age "23"
+    furColour: "orange", //La declaración de la propiedad no debe tener espacio.
+    age: 23, //Faltaban los dos punto, la coma y los números no van en comillas.
 };
 
-var laptop = 
-    brand: "Lenovo"
-    ram "5GB"
-}
+var laptop = { //Abrir corchete
+    brand: "Lenovo", //La coma al final
+    ram: "5GB", //Los dos punto y la coma al final
+}; //Punto y coma al final
 
 var phone = {
-    operating system "iOS",
+    operatingSystem: "iOS", //La declaración de la propiedad no debe tener espacio y faltaban los dos puntos.
     hasStylus: true,
-    megapixels 12
-    "batteryLife": "24 hours"
+    megapixels: 12, //Los dos puntos y la coma
+    batteryLife: "24 hours", //Nombre de la propiedad no va en comillas y la coma al final
+}; //Cerrar corchete y punto y coma.

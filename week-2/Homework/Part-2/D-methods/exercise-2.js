7a1c0d97ce44a81a4c2bbf00fc76ab5a76fe73c1
Maria Victoria Bilbao González <maria.bilbao@icscc.loc>
2021-08-05 12:02:02 +0200

    Agrego una parte del exercise-2-part-2D, debo revisar

--- week-2/Homework/Part-2/D-methods/exercise-2.js
+++ week-2/Homework/Part-2/D-methods/exercise-2.js
@@ -8,10 +8,22 @@ let person = {
     name: "Alice",
     age: 25,
     currentAddress: "Glasgow",
-    changeAddress: (newAddress) {
+    changeAddress: function (newAddress) { //Agrego palabra función
         currentAddress = newAddress;
     },
-    celebrateBirthday: function {
-        that.age = that.age + 1;
-    }
+    celebrateBirthday: function() { //Agrego paréntesis después de la palabra función
+        this.age = this.age + 1; //Se cambia el that por this para hacer referencia al objeto actual del método.
+    },
 };
+
+//Imprime en consola el valor de la función "celebrateBirthday":
+person.celebrateBirthday();
+console.log(person.age);
+
+//Imprime el valor de la función "changeAddress":
+console.log(person.changeAddress());
+
+/* Debo revisar porque me está imprimiendo lo siguiente:
+        26
+        undefined
+*/

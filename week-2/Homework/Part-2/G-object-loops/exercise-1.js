d12f51ddae1705752112bfbdfaad07f2cafffadf
Maria Victoria <marivibilbaog@gmail.com>
2021-08-06 23:39:12 +0200

    Agrego parte de exercise-1-part-2G

--- week-2/Homework/Part-2/G-object-loops/exercise-1.js
+++ week-2/Homework/Part-2/G-object-loops/exercise-1.js
@@ -2,12 +2,22 @@
 // Loop through all the students' grades and console log the name and grade of the ones with grade more than 18
 // Try to use both methods shown above to achieve this
 
-const studentGrades = {
+const studentGrades = { //Ya se encontraba estas líneas.
     tom: 20,
     george: 17,
     abdul: 19,
-  };
-  
+};
+
+//Agrego lo siguiente:
+let values = Object.values(studentGrades); //Declaramos los valores de las propiedades
+let propiedades = Object.keys(studentGrades); //Declaramos las propiedades
+let calificaciones = values.filter(value => value > 18);
+
+console.log(calificaciones);
+/**** REVISAR YA QUE ME DA COMO RESULTADO:
+[ 20, 19 ]
+*/
+
   // Prints
   // TOM - 20
   // ABDUL - 19

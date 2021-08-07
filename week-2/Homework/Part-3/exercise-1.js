7f641f13035658d2b5f5276ec5d11d27154a6359
Maria Victoria <marivibilbaog@gmail.com>
2021-08-07 13:16:24 +0200

    Agrego modificación exercise 1-part-3

--- week-2/Homework/Part-3/exercise-1.js
+++ week-2/Homework/Part-3/exercise-1.js
@@ -35,16 +35,26 @@ let parkAvenueHouse = {
 // returns the full name (first name + last name) of the owner of the house
 function getOwnerFullName(house) {
   return house.currentOwner.firstName + " " + house.currentOwner.lastName;
+  //Otra forma: 
+  //return `${house.currentOwner.firstName} $}{house.currentOwner.lastName}`
 }
 
 // returns an array of the owners' email addresses of the two houses
 function getEmailAddresses(house1, house2) {
   return [house1.currentOwner.email] + " , " + [house2.currentOwner.email];
+  //Otra forma: 
+  //return [house1.currentOwner.email, house2.currentOwner.email];
 }
 
 // returns the address for the cheapest house out of the two
 function getCheapestAddress(house1, house2) {
-  
+  const priceOne = house1.price;
+  const priceTwo = house2.price;
+
+  if(priceOne < priceTwo){
+    return house1.address;
+  }
+  return house2.address;
 }
 
 /*

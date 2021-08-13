e9f024e5b46da8665c2b1edfbdb995b3395738de
Maria Victoria <marivibilbaog@gmail.com>
2021-08-13 21:11:15 +0200

    Agrego modificaciones en slideshow-js-D-slideshow-Homework-Week-3

--- week-3/Homework/D-slideshow/slideshow.js
+++ week-3/Homework/D-slideshow/slideshow.js
@@ -9,12 +9,66 @@ let dogImageFive = "./images/image5.jpg";
 //Array de imágenes
 let dogsImages = [dogImageOne, dogImageTwo, dogImageThree, dogImageFour, dogImageFive];
 
-const img = document.querySelector("img");
+const img = document.querySelector("#imagen");
 
 const autoBackButton = document.getElementById("autoBack");
 const backButton = document.getElementById("back");
 const stopButton = document.getElementById("stop");
-const forwardButton = document.getElementById("forward");
-const autoForwardButton = document.getElementById("autoForward");
+const forwardButton = document.querySelector("#forward");
+const autoForwardButton = document.querySelector("#autoForward");
 
+let posicionActual = 0;
+let intervalo;
+
+//Función para actualizar la imagen dependiendo de la posición actual
+function renderizarImagen(){
+    imagen.style.backgroundImage = `url(${dogsImages[posicionActual]})`;
+};
+
+//Función para siguiente foto
+function forward(){
+    if(posicionActual > dogsImages.length - 1){
+        posicionActual = 0;
+    }else {
+        posicionActual ++;
+    };
+    img.src = dogsImages[posicionActual];
+};
+
+//Función para retroceder foto
+function back(){
+    if(posicionActual <= 0){
+        posicionActual = dogsImages.length -1;
+    }else{
+        posicionActual --;
+    };
+    img.src = dogsImages[posicionActual];
+};
+
+//Agrego evento para botón forward
+forwardButton.addEventListener("click",()=>{
+    forward();
+});
+
+//Agrego evento para botón back
+backButton.addEventListener("click",()=>{
+    back();
+});
+
+//Agrego evento para botón stop
+stopButton.addEventListener("click",()=>{
+    clearInterval(intervalo)
+});
+
+//Agrego evento para botón auto-forward
+autoForwardButton.addEventListener("click",()=>{
+    clearInterval(intervalo)
+    intervalo = setInterval(forward,2000)
+});
+
+//Agrego evento para botón auto-back
+aBackBtn.addEventListener("click",()=>{
+    clearInterval(interval)
+    intervalo = setInterval(back,2000)
+});
 

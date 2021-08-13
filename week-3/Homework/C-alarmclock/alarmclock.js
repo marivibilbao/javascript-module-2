5993ab766a5e9c2706939bc17e5e958aee921865
marivibilbao <marivibilbaog@gmail.com>
2021-08-13 15:38:58 +0200

    Agrego Homework-Week-3-C-alarmclock

--- week-3/Homework/C-alarmclock/alarmclock.js
+++ week-3/Homework/C-alarmclock/alarmclock.js
@@ -1,4 +1,31 @@
-function setAlarm() {}
+let interval;
+
+function setAlarm() { //Creamos función 
+  //Declaramos las variables
+  let alarmSet = document.getElementById("alarmSet").value;
+  let timeRemaining = document.getElementById("timeRemaining");
+  let body = document.querySelector("body");
+
+  //Método clearInterval () - Utilizado para detener el tiempo
+  clearInterval(interval); 
+  pauseAlarm(); //Llamamos función para pausa o desactiva audio de alarma
+
+  // Método: setInterval () - Llama a una función o evalúa una expresión a intervalos específicos (en milisegundos).
+  interval = setInterval(()=>{ 
+    if(alarmSet===0){ //Si el reloj llega a 0
+      timeRemaining.innerText = "Time Remaining: 00:0" + alarmSet;
+      body.style.background = "#BF6BE7"; //Cambia de color el background
+      playAlarm(); //Activa sonido
+      clearInterval(interval); //Se aplica método para detener el tiempo.
+    }else if(alarmSet>10){
+      alarmSet = 10;
+      timeRemaining.innerText = "Time Remaining: 00:0" + alarmSet;
+    }else{
+      timeRemaining.innerText = "Time Remaining: 00:0" + alarmSet;
+      alarmSet --;
+    };
+  },1000);
+};
 
 // DO NOT EDIT BELOW HERE
 

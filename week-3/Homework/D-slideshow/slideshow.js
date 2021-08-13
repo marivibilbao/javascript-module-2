3e76e56218d636c67832a25b048934604cd0254a
Maria Victoria <marivibilbaog@gmail.com>
2021-08-13 19:18:21 +0200

    Agreguna parte de Homework-Week-3-D-Slideshow

--- week-3/Homework/D-slideshow/slideshow.js
+++ week-3/Homework/D-slideshow/slideshow.js
@@ -1,1 +1,20 @@
 // Write your code here
+//Declaración de varibles de imágenes, por separado:
+let dogImageOne = "./images/image1.jpg";
+let dogImageTwo = "./images/image2.jpg";
+let dogImageThree = "./images/image3.jpg";
+let dogImageFour = "./images/image4.jpg";
+let dogImageFive = "./images/image5.jpg";
+
+//Array de imágenes
+let dogsImages = [dogImageOne, dogImageTwo, dogImageThree, dogImageFour, dogImageFive];
+
+const img = document.querySelector("img");
+
+const autoBackButton = document.getElementById("autoBack");
+const backButton = document.getElementById("back");
+const stopButton = document.getElementById("stop");
+const forwardButton = document.getElementById("forward");
+const autoForwardButton = document.getElementById("autoForward");
+
+

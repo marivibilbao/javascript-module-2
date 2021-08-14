1bb84738d418bb3fb0f52bb2288f66ccf7c4ae06
Maria Victoria <marivibilbaog@gmail.com>
2021-08-14 11:09:08 +0200

    Agrego otra forma de hacer el exercise-A-Homework-Week-3

--- week-3/Homework/A-timeout/exercise.js
+++ week-3/Homework/A-timeout/exercise.js
@@ -11,16 +11,44 @@ Update your code to make the colour change every 5 seconds to something differen
 */
 
 //Task 1 ---*** setTimeout()
+/*
 setTimeout(function backgroundFirst() {
     document.body.style.backgroundColor = "#E8B3F3"; },5000); //Cambiamos backgroundColor, después de 5 segundos
 
+*/
+
 //Task 2 ---*** setInterval()
 //Cada 5 segundos cambiar color de fondo
+function generateRandomNumber(){
+    return parseInt(Math.random() * (255 - 0) + 0);
+};
+
+//Otra forma de hacer el ejercicio explicado en clase
+let i = 0; //Declaro variable
+function changeColorsBackground() {
+    //let colors = ["#829DF2", "#5BD48C", "#FFC300", "#C0C0C0", "#D52444", "#C634F8"]; //Arregle de colores
+    document.body.style.backgroundColor = `rgb(${generateRandomNumber()},${generateRandomNumber()},${generateRandomNumber()})`; 
+    //i++;
+    //if(i === colors.length){
+    //    i = 0;
+    //};
+    console.log(generateRandomNumber());
+};
+
+setInterval(changeColorsBackground, 5000);
+
+/*
 let i = 0; //Declaro variable
 function changeColorsBackground() {
     let colors = ["#829DF2", "#5BD48C", "#FFC300", "#C0C0C0", "#D52444", "#C634F8"]; //Arregle de colores
     document.body.style.backgroundColor = colors[i]; 
-    i = (i + 1) % colors.length;
+    i++;
+    if(i === colors.length){
+        i = 0;
+    };
+    //i = (i + 1) % colors.length; //% es el módulo 
+    console.log(i);
 };
 
 setInterval(changeColorsBackground, 5000);
+*/
